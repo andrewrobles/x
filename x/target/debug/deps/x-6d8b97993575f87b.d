88d98479687ebd2c72ae78ae52e1c09cd0c66da7
andrewrobles <156987928+andrewrobles@users.noreply.github.com>
2026-09-12 09:24:42 -0700

    Project setup

---
+++ x/target/debug/deps/x-6d8b97993575f87b.d
@@ -0,0 +1,5 @@
+/Users/andrew/2026/x/x/target/debug/deps/x-6d8b97993575f87b.d: src/main.rs
+
+/Users/andrew/2026/x/x/target/debug/deps/x-6d8b97993575f87b: src/main.rs
+
+src/main.rs:
---
+++ x/target/debug/x.d
@@ -0,0 +1,1 @@
+/Users/andrew/2026/x/x/target/debug/x: /Users/andrew/2026/x/x/src/main.rs
